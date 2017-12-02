7f9eb9cb9b7b854ed0b64d2283a6a47f312cb6d1
ervinpepic <ervin.hack@gmail.com>
2017-12-02 20:55:24 +0100

    Added ability to pick avatar with complete functions

--- Forum/View/AvatarCell.swift
+++ Forum/View/AvatarCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
     
     @IBOutlet weak var avatarImg: UIImageView!
@@ -16,6 +21,21 @@ class AvatarCell: UICollectionViewCell {
         
         super.awakeFromNib()
         setupView()
+        
+    }
+    
+    func configureCell(index: Int, type: AvatarType) {
+        
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+            
+        } else {
+            
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+            
+        }
         
     }
     
